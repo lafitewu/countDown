046a81cad2d23a511259ae0955d30154256a98f2
lafitewu <863512936@qq.com>
2018-08-22 18:39:21 +0800

    coundow更新

--- README.md
+++ README.md
@@ -12,17 +12,19 @@
         times: '2018/8/20 18:00:00',  //必填'2018/8/13 18:00:00或者 2(两分钟) 
         ms: false,   //毫秒是否开启
         Hour: true   //小时是否开启
-    });
+    },callback);
 参数说明：
 	times: 必填。 两种形式可选('2018/8/20 18:00:00'和 number) number代表分钟。 比如3，则代表3分钟倒计时。
 	Hour: boolean 默认true  小时开关
 	ms: boolean  默认false  毫秒开关
 	msValue: number  默认90，当ms为true时生效。
+	callback  function  是倒计时完成后的回调函数。
 
 ## 作者&&插件简介 ##
 name: laftiewu <br>
 qq: 863512936 <br>
 v：1.0 <br>
 creatTime: 2018.08.14 <br>
-update: 2018.08.20 <br>
+update: 2018.08.22 <br>
+2018.08.22新增callback回调函数
    
--- countDown.js
+++ countDown.js
@@ -10,10 +10,12 @@
         times: '2018/8/20 18:00:00',  //必填'2018/8/13 18:00:00或者 2(两分钟) 
         ms: false,   //毫秒是否开启
         Hour: true   //小时是否开启
-    });
+    },callback);
+
+    callback是倒计时完成后的回调函数。
 */
 
-$.fn.countDown = function(options) {
+$.fn.countDown = function(options,callback) {
 	var _self = this;
 	var defaults={
 		times: '', //必填参数
@@ -21,7 +23,7 @@ $.fn.countDown = function(options) {
 		ms: false, //毫秒开关
 		msValue: 90 //毫秒间隔
     };
-    var endOptions=$.extend(defaults,options);
+    var endOptions=$.extend(defaults,options,callback);
 
     if(!endOptions.times) {
     	console.error("times值必填");
@@ -67,7 +69,9 @@ $.fn.countDown = function(options) {
  				// 倒计时结束触发
  				if(this.Hours <= 0 && this.Minutes <= 0 && this.Seconds <= 0) {
  					console.log("倒计时结束");
+                    callback();
  					clearInterval(this.ClearIn);
+
  				}
     		},
     		countFn:function() {
