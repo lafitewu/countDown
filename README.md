e8d7c59f177ae332ab2340039f3435d080b6dc39
lafitewu <your_emal>
2019-05-09 18:55:34 +0800

    1.2版本

--- README.md
+++ README.md
@@ -10,13 +10,22 @@
 
     $(".main").countDown({
         times: '2018/8/20 18:00:00',  //必填'2018/8/13 18:00:00或者 2(两分钟) 
+		days: true,  //天数显示
         ms: false,   //毫秒是否开启
-        Hour: true   //小时是否开启
+        Hour: true,   //小时是否开启
+		unit: {
+			days: ':',
+			hour: ':',
+			min: ':',
+			second: ':'
+		}              //默认都是":"  可依次修改成：天、时、分、秒 看看效果
     },callback);
 参数说明：
 	``times:`` 必填。 两种形式可选('2018/8/20 18:00:00'和 number) number代表分钟。 比如3，则代表3分钟倒计时。<br>
+	``days:`` boolean 默认true  天数开关<br/>
 	``Hour:`` boolean 默认true  小时开关<br/>
 	``ms:`` boolean  默认false  毫秒开关<br/>
+	``unit:`` object 默认值全为“:”, 可自行修改<br/>
 	``msValue:`` number  默认90，当ms为true时生效。<br/>
 	``callback``  function  是倒计时完成后的回调函数。
 
@@ -25,7 +34,9 @@ name: laftiewu <br/>
 qq: 863512936 <br/>
 v：1.0 <br/>
 creatTime: 2018.08.14 <br/>
-update: 2018.08.23 <br/>
-2018.08.22新增callback回调函数<br/>
-2018.08.23新增默认回调函数&&压缩版修护
+update: 2019.05.09  v1.2<br/>
+--- 2018.08.22新增callback回调函数<br/>
+--- 2018.08.23新增默认回调函数&&压缩版修护<br/>
+--- 2019.05.09新增days参数，是否开启天数倒计时 && 新增unit对象可修改默认“:”连接展示，同时每个元素新增样式，可自己修改样式属性<br/>
+--- ps: 自行修改样式，可到控制台查看元素样式
    
--- countDown.js
+++ countDown.js
@@ -1,27 +1,46 @@
 /*
     by:lafitewu
-    time: 2018.08.14
+    creatTime: 2018.08.14
     qq:863512936
     v: 1.0
 
+	update: 2019.05.09  v1.2
+	--- 2018.08.22新增callback回调函数
+	--- 2018.08.23新增默认回调函数&&压缩版修护
+	--- 2019.05.09新增days参数，是否开启天数倒计时 && 新增unit对象可修改默认“:”连接展示，同时每个元素新增样式，可自己修改样式属性
 
     ps:
     $(".main").countDown({
-        times: '2018/8/20 18:00:00',  //必填'2018/8/13 18:00:00或者 2(两分钟) 
+		times: '2018/8/20 18:00:00',  //必填'2018/8/13 18:00:00或者 2(两分钟) 
+		days: true, //天数显示
         ms: false,   //毫秒是否开启
-        Hour: true   //小时是否开启
+		Hour: true,   //小时是否开启
+		unit: {
+			days: ':',
+			hour: ':',
+			min: ':',
+			second: ':'
+		}
     },callback);
 
-    callback是倒计时完成后的回调函数。
+	callback是倒计时完成后的回调函数。
+	注意：当天数开关开启时，小时开关也将自动开启
 */
 
 $.fn.countDown = function(options,callback) {
 	var _self = this;
 	var defaults={
 		times: '', //必填参数
+		days: true, //天数显示
 		Hour: true, // 小时显示 
 		ms: false, //毫秒开关
-		msValue: 90 //毫秒间隔
+		msValue: 90, //毫秒间隔
+		unit: {
+			days: ':',
+			hour: ':',
+			min: ':',
+			second: ':'
+		}
     };
     var endOptions=$.extend(defaults,options,callback);
     function demo() {
@@ -41,7 +60,8 @@ $.fn.countDown = function(options,callback) {
     	timeFn.prototype = {
     		Init:function() {
     			this.time = new Date().getTime();
-    			this.time_diff = (this.End_time - this.time);
+				this.time_diff = (this.End_time - this.time);
+				this.days = parseInt(this.time_diff/(1000 * 60 * 60 * 24));
     			this.Hours = parseInt(this.time_diff/(1000 * 60 * 60));
     			this.Minutes = parseInt(this.time_diff/(1000 * 60));
     			this.Seconds = (this.time_diff/1000)%60;
@@ -53,20 +73,36 @@ $.fn.countDown = function(options,callback) {
     			}else {
     				return x
     			}
-    		},
+			},
+			// 特殊值处理
+			special:function(e,de) {
+				if(e == undefined) {
+					return e = de;
+				}else {
+					return e;
+				}
+			},
     		showText:function() {
     			if(endOptions.ms) {
-					if(endOptions.Hour) {
-    					_self.text(this.AddZero(this.Hours) + ":" + this.AddZero(this.Minutes%60)+":"+this.AddZero(this.Seconds.toFixed(3)));
-    				}else {
-    					_self.text(this.AddZero(this.Minutes%60)+":"+this.AddZero(this.Seconds.toFixed(3)));
-    				}
+					if(endOptions.days) {
+						_self.html('<span class="Days_val">' + this.AddZero(this.days) + '</span>' + '<span class="Days_unit">' + this.special(endOptions.unit.days,':') + '</span>' + '<span class="Hour_val">' + this.AddZero(this.Hours%24) + '</span>' + '<span class="Hour_unit">' + this.special(endOptions.unit.hour,":") + '</span>' + '<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,':') + '</span>' + '<span class="Second_val">' + this.AddZero(this.Seconds.toFixed(3)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,'') + '</span>');
+					}else {
+						if(endOptions.Hour) {
+							_self.html('<span class="Hour_val">' + this.AddZero(this.Hours) + '</span>' + '<span class="Hour_unit">' + this.special(endOptions.unit.hour,":") + '</span>' + '<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,':') + '</span>' + '<span class="Second_val">' + this.AddZero(this.Seconds.toFixed(3)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,'') + '</span>');
+						}else {
+							_self.html('<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,':') + '</span>' + '<span class="Second_val">' + this.AddZero(this.Seconds.toFixed(3)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,'') + '</span>');
+						}
+					}
  				}else {
- 					if(endOptions.Hour) {
-    					_self.text(this.AddZero(this.Hours) + ":" +this.AddZero(this.Minutes%60)+":"+this.AddZero(parseInt(this.Seconds)));
-    				}else {
-    					_self.text(this.AddZero(this.Minutes%60)+":"+this.AddZero(parseInt(this.Seconds)));
-    				}
+					if(endOptions.days) {
+						_self.html('<span class="Days_val">' + this.AddZero(this.days) + '</span>' + '<span class="Days_unit">' + this.special(endOptions.unit.days,":") + '</span>' + '<span class="Hour_val">' + this.AddZero(this.Hours%24) + '</span>' + '<span class="Hour_unit">' + this.special(endOptions.unit.hour,":") + '</span>' + '<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,":") + '</span>' + '<span class="Second_val">' + this.AddZero(parseInt(this.Seconds)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,"") + '</span>');
+					}else {
+						if(endOptions.Hour) {
+							_self.html('<span class="Hour_val">' + this.AddZero(this.Hours) + '</span>' + '<span class="Hour_unit">' + this.special(endOptions.unit.hour,":") + '</span>' + '<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,":") + '</span>' + '<span class="Second_val">' + this.AddZero(parseInt(this.Seconds)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,"") + '</span>');
+						}else {
+							_self.html('<span class="Min_val">' + this.AddZero(this.Minutes%60) + '</span>' + '<span class="Min_unit">' + this.special(endOptions.unit.min,":") + '</span>' + '<span class="Second_val">' + this.AddZero(parseInt(this.Seconds)) + '</span>' + '<span class="second_unit">' + this.special(endOptions.unit.second,"") + '</span>');
+						}
+					}
  				}
 
  				// 倒计时结束触发
